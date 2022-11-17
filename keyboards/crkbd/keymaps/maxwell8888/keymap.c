f35e6959b84f109b8a51c489b9511a2c65b9748b
Max Unsted <maxunsted@gmail.com>
2022-11-17 16:01:06 +0000

    1st attempt

--- keyboards/crkbd/keymaps/maxwell8888/keymap.c
+++ keyboards/crkbd/keymaps/maxwell8888/keymap.c
@@ -20,28 +20,16 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #include <stdio.h>
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-      [0] = LAYOUT_split_3x6_3(
+  [0] = LAYOUT_split_3x6_3(
   //,-----------------------------------------------------.                    ,-----------------------------------------------------.
        KC_ESC,    KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                         KC_Y,    KC_U,    KC_I,    KC_O,   KC_P,  KC_BSPC,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-       KC_TAB,    LSFT_T(KC_A), LCTL_T(KC_S), KC_D,   KC_F,    KC_G,            KC_H,    KC_J, RGUI_T(KC_K), RCTL_T(KC_L), RSFT_T(KC_SCLN), KC_QUOT,
+       KC_TAB,    KC_A, KC_S, KC_D,   KC_F,    KC_G,                                KC_H,    KC_J,    KC_K,    KC_L, KC_SCLN, KC_QUOT,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-       KC_GRV,    KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,                         KC_N,    KC_M, KC_COMM,  KC_DOT, KC_SLSH, KC_NUBS,
+LSFT_T(KC_GRV),   KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,                         KC_N,    KC_M, KC_LEFT,   KC_UP, KC_DOWN, RSFT_T(KC_RIGHT),
   //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
-                                            MO(3),   MO(1),  KC_SPC,     KC_ENT,   MO(2),    MO(3)
+                                          KC_LGUI, KC_LALT,  KC_SPC,     KC_ENT,   KC_RCTL,  MO(3)
                                       //`--------------------------'  `--------------------------'
-
-  ),
-//   [0] = LAYOUT_split_3x6_3(
-//   //,-----------------------------------------------------.                    ,-----------------------------------------------------.
-//        KC_ESC,    KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                         KC_Y,    KC_U,    KC_I,    KC_O,   KC_P,  KC_BSPC,
-//   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-//        KC_TAB,    KC_A, KC_S, KC_D,   KC_F,    KC_G,                        KC_H,    KC_J, KC_K, RCTL_T(KC_L), KC_SCLN, KC_QUOT,
-//   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-//        KC_GRV,    KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,                         KC_N,    KC_M, KC_COMM,  KC_DOT, KC_SLSH, KC_NUBS,
-//   //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
-//                                             MO(3),   MO(1),  KC_SPC,     KC_ENT,   MO(2),    MO(3)
-//                                       //`--------------------------'  `--------------------------'
 
 //   ),
 
@@ -107,11 +95,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         //  KC_1,    KC_2, KC_INT3,    KC_4,    KC_5,    KC_6,                         KC_7,    KC_8,    KC_9,    KC_0, KC_MINS,  KC_EQL,
          KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,                         KC_7,    KC_8,    KC_9,    KC_0, KC_MINS,  KC_EQL,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-        KC_NO, KC_LSFT, KC_LCTL, KC_LGUI, KC_LALT,   KC_NO,                      KC_HOME,  KC_RCTL, KC_UP, KC_PAGE_UP, KC_LBRC, KC_RBRC,
+        KC_NO, KC_LSFT, KC_LCTL, KC_LGUI, KC_LALT,   KC_NO,                  KC_HOME, KC_PAGE_UP, KC_PAGE_DOWN, KC_END, KC_LBRC, KC_RBRC,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-        KC_DELETE,   KC_F2,   KC_F8,  KC_F11,  KC_F12,   KC_NO,                KC_NO, KC_LEFT, KC_DOWN, KC_RIGHT, KC_PAGE_DOWN, KC_END,
+RSFT_T(KC_DEL),  KC_F2,   KC_F8,  KC_F11,  KC_F12,   KC_NO,                        KC_NO,   KC_NO, KC_COMM,  KC_DOT, KC_SLSH, RSFT_T(KC_NUBS),
   //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
-                                            KC_NO, _______,   KC_NO,      KC_NO, _______,    KC_NO
+                                          KC_LGUI, KC_LALT,  KC_SPC,     KC_ENT, KC_RCTL, _______,
                                       //`--------------------------'  `--------------------------'
   )
 };
