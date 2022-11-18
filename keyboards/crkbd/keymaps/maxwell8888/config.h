36c08032f9657838fd647a630b1f4084ed2cb129
Max Unsted <maxunsted@gmail.com>
2022-11-18 12:11:01 +0000

    update

--- keyboards/crkbd/keymaps/maxwell8888/config.h
+++ keyboards/crkbd/keymaps/maxwell8888/config.h
@@ -29,8 +29,8 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 // #define EE_HANDS
 
 //#define TAPPING_FORCE_HOLD
-// #define TAPPING_TERM 400
-// #define IGNORE_MOD_TAP_INTERRUPT
+#define TAPPING_TERM 400
+#define IGNORE_MOD_TAP_INTERRUPT
 
 #ifdef RGBLIGHT_ENABLE
     #define RGBLIGHT_EFFECT_BREATHING
